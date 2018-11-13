69f29c388eae857497843cca4e3d924efe0c615b
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-11-13 18:25:27 +0100

    Remove doze bypass as it is no longer necessary

--- app/src/main/kotlin/app/settings/SettingsPreferenceFragmentCompat.kt
+++ app/src/main/kotlin/app/settings/SettingsPreferenceFragmentCompat.kt
@@ -4,11 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.startIntent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.PowerManager
-import android.provider.Settings
-import android.support.annotation.RequiresApi
 import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
 import domain.autoswipe.ImmediatePostAutoSwipeUseCase
@@ -19,33 +15,10 @@ import org.stoyicker.dinger.R
 internal class SettingsPreferenceFragmentCompat : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.prefs_settings, rootKey)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            initializeBypassDozePreference()
-        }
         addTriggerToAutoswipeEnabledPreference()
         initializeSharePreference()
         initializeAboutTheAppPreference()
     }
-
-    @SuppressLint("BatteryLife")
-    @RequiresApi(Build.VERSION_CODES.M)
-    private fun initializeBypassDozePreference() {
-        findPreference(context?.getString(R.string.preference_key_bypass_doze))
-                ?.takeIf { Build.VERSION.SDK_INT >= Build.VERSION_CODES.M }
-                ?.onPreferenceClickListener = Preference.OnPreferenceClickListener {
-            val packageName = context?.packageName
-            val intent = if (context?.getSystemService(PowerManager::class.java)
-                            ?.isIgnoringBatteryOptimizations(packageName) == true) {
-                Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
-            } else {
-                Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
-                        .setData(Uri.parse("package:$packageName"))
-            }
-            context?.startIntent(intent)
-            true
-        }
-    }
-
 
     private fun addTriggerToAutoswipeEnabledPreference() {
         findPreference(context?.getString(R.string.preference_key_autoswipe_enabled))
