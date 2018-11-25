1ba952fc4ab60c20422c28cdc99575f6a0bebd1c
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-11-25 20:00:58 +0100

    Finish implementing first version of seen

--- app/src/main/kotlin/app/seen/SeenFragment.kt
+++ app/src/main/kotlin/app/seen/SeenFragment.kt
@@ -1,20 +1,40 @@
 package app.seen
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.arch.paging.PagedList
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
+import app.app.seen.SeenAdapter
+import domain.recommendation.DomainRecommendationUser
+import domain.seen.SeenRecommendationsViewModel
+import org.stoyicker.dinger.R
 
 internal class SeenFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
+                              parent: ViewGroup?,
                               savedInstanceState: Bundle?) =
-            FrameLayout(context!!).also {
-                it.background = ColorDrawable(Color.GREEN)
-            }
+            inflater.inflate(R.layout.fragment_seen, parent, false)!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val seenAdapter = SeenAdapter()
+        ViewModelProviders.of(this).get(SeenRecommendationsViewModel::class.java).filter().observe(
+                this@SeenFragment, Observer<PagedList<DomainRecommendationUser>> {
+            seenAdapter.submitList(it)
+        })
+        (view as RecyclerView).apply {
+            adapter = seenAdapter
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+                    .also {
+                        it.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+                    }
+        }
+    }
 
     companion object {
         fun newInstance() = SeenFragment()
--- data/src/main/kotlin/data/seen/SeenRecommendationsModule.kt
+++ data/src/main/kotlin/data/seen/SeenRecommendationsModule.kt
@@ -4,12 +4,13 @@ import dagger.Module
 import dagger.Provides
 import data.tinder.recommendation.RecommendationUserResolver
 import data.tinder.recommendation.RecommendationUserResolverModule
+import domain.seen.SeenRecommendations
 import javax.inject.Singleton
 
 @Module(includes = [RecommendationUserResolverModule::class])
 internal class SeenRecommendationsModule {
     @Provides
     @Singleton
-    fun seenRecommendations(resolver: RecommendationUserResolver) =
+    fun seenRecommendations(resolver: RecommendationUserResolver): SeenRecommendations =
             SeenRecommendationsImpl(resolver)
 }
