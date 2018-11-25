2f24e8158f335b4f6a3933ca801ef8c739f80212
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-11-25 19:45:25 +0100

    Complete bringing up seen to the app layer

--- app/src/main/kotlin/app/seen/SeenViewHolder.kt
+++ app/src/main/kotlin/app/seen/SeenViewHolder.kt
@@ -2,14 +2,18 @@ package app.app.seen
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.TextView
 import domain.recommendation.DomainRecommendationUser
 
 class SeenViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bindTo(item: DomainRecommendationUser) {
+        itemView.visibility = View.VISIBLE
+        (itemView as TextView).text = item.name
         // TODO Set attributes to values and progress to GONE
     }
 
     fun clear() {
+        itemView.visibility = View.GONE
         // TODO Set attributes to empty and progress to VISIBLE
     }
 }
--- domain/src/main/kotlin/domain/seen/SeenRecommendationsViewModel.kt
+++ domain/src/main/kotlin/domain/seen/SeenRecommendationsViewModel.kt
@@ -1,23 +1,12 @@
 package domain.seen
 
-import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
 import android.arch.paging.LivePagedListBuilder
-import android.arch.paging.PagedList
-import domain.recommendation.DomainRecommendationUser
 
 class SeenRecommendationsViewModel() : ViewModel() {
-    private lateinit var list: LiveData<PagedList<DomainRecommendationUser>>
-
-    fun filter() = list
-
-    private fun filterInternal(filter: String) {
-        list = LivePagedListBuilder(
-                SeenRecommendationsHolder.seenRecommendations.filter(filter),
-                PAGE_SIZE)
-                .build()
-    }
-    // TODO Continue here: https://developer.android.com/topic/libraries/architecture/paging/#ex-observe-livedata
+    fun filter(filter: String = "") = LivePagedListBuilder(
+            SeenRecommendationsHolder.seenRecommendations.filter(filter), PAGE_SIZE)
+            .build()
 }
 
 private const val PAGE_SIZE = 25
