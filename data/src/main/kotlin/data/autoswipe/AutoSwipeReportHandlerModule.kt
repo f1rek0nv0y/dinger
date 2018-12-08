ccc480440f47443570c9fe0514dbb4b294fb00ca
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2018-12-08 09:25:45 +0100

    Remove micro-optimization lint warning

--- data/src/main/kotlin/data/autoswipe/AutoSwipeReportHandlerModule.kt
+++ data/src/main/kotlin/data/autoswipe/AutoSwipeReportHandlerModule.kt
@@ -1,21 +1,15 @@
 package data.autoswipe
 
-import android.content.SharedPreferences
 import dagger.Module
 import dagger.Provides
 import data.notification.NotificationManager
 import data.notification.NotificationManagerModule
-import data.preferences.DefaultSharedPreferencesModule
 import javax.inject.Singleton
 
-@Module(includes = [
-    NotificationManagerModule::class,
-    DefaultSharedPreferencesModule::class])
+@Module(includes = [NotificationManagerModule::class])
 internal class AutoSwipeReportHandlerModule {
     @Provides
     @Singleton
-    fun autoSwipeReportHandler(
-            defaultSharedPreferences: SharedPreferences,
-            notificationManager: NotificationManager) =
+    fun autoSwipeReportHandler(notificationManager: NotificationManager) =
             AutoSwipeReportHandler(notificationManager)
 }
