ff7b77aee9f6f8714d35669a8c0af64cd92e1be3
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2019-02-03 14:31:04 +0100

    Show teasers in seen

--- app/src/main/kotlin/app/seen/SeenViewHolder.kt
+++ app/src/main/kotlin/app/seen/SeenViewHolder.kt
@@ -5,10 +5,20 @@ import android.view.View
 import domain.recommendation.DomainRecommendationUser
 import kotlinx.android.synthetic.main.item_view_recommendation.view.name
 import kotlinx.android.synthetic.main.item_view_recommendation.view.picture
+import kotlinx.android.synthetic.main.item_view_recommendation.view.teaser
 
 class SeenRecommendationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
   fun bindTo(item: DomainRecommendationUser) {
     itemView.name.text = item.name
+    itemView.teaser.apply {
+      val desc = item.teaser.description
+      if (desc.isEmpty()) {
+        visibility = View.GONE
+      } else {
+        visibility = View.VISIBLE
+        text = desc
+      }
+    }
     itemView.picture.loadImage(item.photos.firstOrNull()?.url)
   }
 
